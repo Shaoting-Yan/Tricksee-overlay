71d2fc11814774fb8c2797c767640836c1fdf599
Shaoting-Yan <112460516+Shaoting-Yan@users.noreply.github.com>
2023-10-16 21:25:12 +0800

    use svg

--- src/module/sketches/Visual.jsx
+++ src/module/sketches/Visual.jsx
@@ -27,7 +27,6 @@ function sketch(p5){
     p5.vw = wrapper.clientWidth;
     p5.vh = wrapper.clientHeight;
     p5.createCanvas(p5.vw,p5.vh,p5.WEBGL); 
-    p5.mountain = p5.loadImage("mountain.png");
     //view variables
     p5.camHeight = p5.height/2/p5.tan(p5.PI/6);
     //motion parameter
@@ -43,12 +42,56 @@ function sketch(p5){
     p5.Ry = rotation[2];
     p5.Rx = rotation[1];  
     p5.clear();
-    p5.background("red");
     p5.moveCamera();
     p5.moveObject();
     p5.noStroke();
-    p5.box(100);
-    p5.image(p5.mountain,0,-100);
+    p5.translate(0,-100,0);
+    //mountain svg;
+    p5.fill("rgba(0, 0, 0, 0)")
+    p5.beginShape();
+    p5.vertex(0,0);
+    p5.vertex(4224,0);
+    p5.vertex(4224,1066);
+    p5.vertex(0,1066);
+    p5.endShape();
+    p5.stroke('rgba(0,0,0,0)')
+    p5.strokeCap(p5.PROJECT);
+    p5.strokeJoin(p5.MITER);
+    p5.fill("#ffffff")
+    p5.beginShape();
+    p5.vertex(185.2,35.2);
+    p5.vertex(0,220.4);
+    p5.vertex(0,1066);
+    p5.vertex(4224,1066);
+    p5.vertex(4224,252.4);
+    p5.vertex(4038.8,67.2);
+    p5.vertex(3948,158);
+    p5.vertex(3790,0);
+    p5.vertex(3537.6,252.4);
+    p5.vertex(3352.4,67.2);
+    p5.vertex(3167.2,252.4);
+    p5.vertex(2950,35.2);
+    p5.vertex(2796.1,189.1);
+    p5.vertex(2724.2,117.2);
+    p5.vertex(2589,252.4);
+    p5.vertex(2403.8,67.2);
+    p5.vertex(2281.9,189.1);
+    p5.vertex(2128,35.2);
+    p5.vertex(1910.8,252.4);
+    p5.vertex(1775.6,117.2);
+    p5.vertex(1640.4,252.4);
+    p5.vertex(1423.2,35.2);
+    p5.vertex(1269.3,189.1);
+    p5.vertex(1147.4,67.2);
+    p5.vertex(1025.5,189.1);
+    p5.vertex(953.6,117.2);
+    p5.vertex(818.4,252.4);
+    p5.vertex(566,0);
+    p5.vertex(408,158);
+    p5.vertex(317.2,67.2);
+    p5.vertex(267.2,117.2);
+    p5.vertex(185.2,35.2);
+    p5.endShape();
   }
 }
 
